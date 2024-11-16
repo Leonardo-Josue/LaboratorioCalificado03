5719bce9852755236186eddccc2560590dbb3341
Leonardo-Josue <josue.chavez.q@tecsup.edu.pe>
2024-11-16 11:29:11 -0500

    first commit

---
+++ app/src/main/java/com/chavez/leonardo/laboratoriocalificado03/Teacher.kt
@@ -0,0 +1,11 @@
+package com.chavez.leonardo.laboratoriocalificado03
+
+data class Teacher(
+    val name: String,
+    val last_name: String,
+    val phone_number: String,
+    val email: String,
+    val image_url: String
+)
+
+
---
+++ app/src/main/java/com/chavez/leonardo/laboratoriocalificado03/TeacherResponse.kt
@@ -0,0 +1,5 @@
+package com.chavez.leonardo.laboratoriocalificado03
+
+data class TeacherResponse(
+    val teachers: List<Teacher>
+)
